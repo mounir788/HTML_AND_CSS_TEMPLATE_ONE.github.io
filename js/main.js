472efc1eec830a2e0a610869b6161640da25fa94
Mounir Ahmed <86005983+mounir788@users.noreply.github.com>
2022-01-04 02:38:26 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,8 +5,24 @@ let numbers = document.querySelectorAll(".number");
 let started = false;
 let menu = document.querySelector(".bars");
 let menulist = document.querySelector(".bars + ul");
+let toTop = document.querySelector(".toTop");
+
+toTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
 menu.onclick = () => menulist.classList.add("show");
+
 window.onscroll = function () {
+  if (this.scrollY >= 600) {
+    toTop.classList.add("visible");
+  } else {
+    toTop.classList.remove("visible");
+  }
+
   if (this.scrollY >= skillsSection.offsetTop) {
     spans.forEach((span) => {
       span.style.width = span.dataset.progress;
